120c680396120aeb5bc8e05fff9b6f9c1e5552cf
Jason Persson <jason.persson@pz.com.au>
2024-07-03 12:03:53 +1000

    Change font for Neovide

--- init.lua
+++ init.lua
@@ -93,6 +93,11 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Neovide stuff
+if vim.g.neovide then
+  vim.o.guifont = 'FiraCode Nerd Font:h10'
+end
+
 -- [[ Setting options ]]
 require 'options'
 
